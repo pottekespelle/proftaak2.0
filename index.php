9aa87144b96bd5437ddbb7516415d67ea232f847
Unknown <VisQuinten@gmail.com>
2017-05-16 10:29:31 +0200

    New Repository
    New Repository

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Proftaak</title>
+	<link rel="stylesheet" type="text/css" href="http://meyerweb.com/eric/tools/css/reset/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+<div>
+	<div class="header">
+        <a href="#"><img src="img/logo.png" class="logo"></a>
+        <a><img src="img/login.png" class="login"></a>
+    </div>
+
+	<div class="home-content">
+    	<h2>CSHUB</h2>
+    	<p>Gag ipsum dolar sit amet read pokeman all the things no grey drink lose. Russia easter bra like a boss mom no bad female morbi. Genius really? People jackie chan pizza pikachu 9000 nap bart cat good guy i dont get it. Top party sit viverra clinton vegan bear iron man loki. Superhero in class bacon in cereal guy troll hehexd laptop tank thor cuteness overload collection. Happy left luke blizzard cellphone hac yao rainbow a feel like a sir. Not bad humor note amnesia mother le girlfriend derp movie right why angry birds. Money close enough high school ipsum facebook cool brother win le derp y u no.</p>
+    </div>
+	<div class="wrapper">
+		<div class="form-input">
+			<label class="icon glyphicon glyphicon-pencil"></label>
+			<button id="convert" class="button glyphicon glyphicon-search"></button>
+			<input type="text" class="input" value="76561198344278706" />
+		</div>
+	</div>
+</div>
+</body>
+</html>
